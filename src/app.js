ea68f1f0771338193b5fab143ac9d67fedb2e265
eideard-hm <ehhernandez81@misena.edu.co>
2021-11-23 16:50:01 -0500

    Move to directory view into the src directory

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-import exphbs from "express-handlebars";
+import { create } from "express-handlebars";
 import path from "path";
 
 import router from "./routes/index.routes";
@@ -7,25 +7,22 @@ import router from "./routes/index.routes";
 // objeto de express
 const app = express();
 
-// configurar la ruta de las vistas
+// configurar la ruta de las vistas de handlebars
 app.set("views", path.join(__dirname, "views"));
 
-// configurar el engine template de handlebars
-// .engine(extension, engine): Sirve para configurar el motor de plantillas.
-app.engine(
-  ".hbs",
-  exphbs({
-    // configuraciones
-    // directorio donde se va a guardar las template del proyecto
-    layoutsDir: path.join(app.get("views"), "layouts"),
-    // Fichero que contiene el layout del proyecto
-    defaulLayout: "main",
-    // extensión de las templates que vamos a utilizar
-    extname: ".hbs",
-  })
-);
+// configurar el motor de plantillas handlebars
+const exphbs = create({
+  layoutsDir: path.join(app.get("views"), "layouts"),
+  defaultLayout: "main",
+  partialsDir: path.join(app.get("views"), "partials"),
+  extname: ".hbs",
+});
 
-// decirle a express que use la configuración del engine de handlebars
+
+// Registrar la devolución de llamada del motor de plantilla dada
+app.engine(".hbs", exphbs.engine);
+
+// decirle a express que use la configuración de motor de plantilla
 app.set("view engine", ".hbs");
 
 // usar el archivo de rutas
