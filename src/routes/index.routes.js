5374e85ab74fe9e75fa88bce1ccc3c390d584ec0
eideard-hm <ehhernandez81@misena.edu.co>
2021-11-24 11:03:50 -0500

    Added the partials for the tasks and created the route for the find all registers

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,15 @@ import Task from "../models/Task";
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  try {
+    // lean() -> return a plain javascript object
+    // retorne objetos de js tipicos para recorrerlos facilmente
+    const tasks = await Task.find().lean();
+    res.render("index", {tasks: tasks});
+  } catch (error) {
+    res.status(500).send(error);
+  }
   res.render("index");
 });
 
@@ -12,15 +20,6 @@ router.post("/task/add", async (req, res) => {
   try {
     await Task.create(req.body);
     res.redirect("/");
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-router.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    res.send(tasks);
   } catch (error) {
     res.status(500).send(error);
   }
