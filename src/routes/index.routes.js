16384de89d1134509288621f433a5cd942cd4cb2
eideard-hm <ehhernandez81@misena.edu.co>
2021-11-23 21:17:06 -0500

    Route for the find all regiters in the database

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -7,10 +7,20 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
+// ruta para crear una tarea
 router.post("/task/add", async (req, res) => {
   try {
     await Task.create(req.body);
     res.redirect("/");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.send(tasks);
   } catch (error) {
     res.status(500).send(error);
   }
