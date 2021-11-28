a20b81036beb47a5abf8f2004df2a3f53a077521
eideard-hm <ehhernandez81@misena.edu.co>
2021-11-28 17:49:49 -0500

    Added the controller in the app

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,63 @@
+import Task from "../models/Task";
+
+export const renderTasks = async (req, res) => {
+  try {
+    // lean() -> return a plain javascript object
+    // retorne objetos de js tipicos para recorrerlos facilmente
+    const tasks = await Task.find().lean();
+    res.render("index", { tasks: tasks });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+  res.render("index");
+};
+
+export const createTask = async (req, res) => {
+  try {
+    await Task.create(req.body);
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+export const renderTaskEdit = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).lean();
+    res.render("edit", { task });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const editTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Task.findByIdAndUpdate(id, req.body);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Task.findByIdAndDelete(id);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const toggleDone = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    task.done = !task.done;
+    await task.save();
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,69 +1,26 @@
 import { Router } from "express";
-import Task from "../models/Task";
+import {
+  createTask,
+  deleteTask,
+  editTask,
+  renderTaskEdit,
+  renderTasks,
+  toggleDone,
+} from "../controllers/task.controller";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  try {
-    // lean() -> return a plain javascript object
-    // retorne objetos de js tipicos para recorrerlos facilmente
-    const tasks = await Task.find().lean();
-    res.render("index", { tasks: tasks });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-  res.render("index");
-});
+router.get("/", renderTasks);
 
 // ruta para crear una tarea
-router.post("/task/add", async (req, res) => {
-  try {
-    await Task.create(req.body);
-    res.redirect("/");
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+router.post("/task/add", createTask);
 
-router.get("/edit/:id", async (req, res) => {
-  try {
-    const task = await Task.findById(req.params.id).lean();
-    res.render("edit", { task });
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.get("/edit/:id", renderTaskEdit);
 
-router.post("/edit/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    await Task.findByIdAndUpdate(id, req.body);
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.post("/edit/:id", editTask);
 
-router.get("/delete/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    await Task.findByIdAndDelete(id);
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.get("/delete/:id", deleteTask);
 
-router.get("/toggleDone/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const task = await Task.findById(id);
-    task.done = !task.done;
-    await task.save();
-    res.redirect("/");
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.get("/toggleDone/:id", toggleDone);
 
 export default router;
